fce237cbe55a417a20e8b43b3ffd92aff6232fdd
Anuj <anujdt21@gmail.com>
2025-04-17 20:54:15 +0530

    Added catppuccin and indent blankline plugline

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "catppuccin/nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+  },
+}
