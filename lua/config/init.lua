574fa93b3107629e906793d530a67a991925a866
Anuj <anant@Anuj>
2025-04-21 21:16:07 +0530

    Added clangd config. Default to catppuccin

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,1 +1,11 @@
 vim.cmd.colorscheme("catppuccin")
+
+-- Toggle number and relativenumber with ,n
+vim.keymap.set("n", ",n", function()
+  vim.wo.number = not vim.wo.number
+end, { desc = "Toggle line numbers" })
+
+-- Toggle relative number with .n
+vim.keymap.set("n", ".n", function()
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end, { desc = "Toggle relative line numbers" })
