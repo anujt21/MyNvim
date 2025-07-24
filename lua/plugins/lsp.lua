3d7846adda05d09abf6677e63cf2010d1d2954db
Anuj <anuj@axisbinc.com>
2025-07-24 10:01:28 +0000

    Added symbols outline plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,16 @@
 return {
-    "mason-org/mason.nvim",
-    "mason-org/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
+	"mason-org/mason.nvim",
+	"mason-org/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	{
+		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
+		keys = { -- Example mapping to toggle outline
+			{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		},
+		opts = {
+			-- Your setup opts here
+		},
+	},
 }
